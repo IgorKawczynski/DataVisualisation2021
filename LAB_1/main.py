08b06a8bcd1c8ad3ef1f01d7661a21c455f7e61d
Igor <kluseczkibabuni94@gmail.com>
2021-02-25 22:41:07 +0100

    Doing homework for LAB1...

--- LAB_1/main.py
+++ LAB_1/main.py
@@ -1,4 +1,101 @@
 
+from math import *
 
-a = '25.02.2021'
-print('Dzisiejsza data to : %s' %(a))
+# a = '25.02.2021'
+# print('Dzisiejsza data to : %s' %(a))
+
+
+
+#zad1
+
+# a = 5.6
+# b = 'napis'
+# print(a, '%s' %(b))
+
+#zad2
+
+# a = 6
+# b = 8
+# dodawanie = a + b
+# odejmowanie = a - b
+# dzielenie = a / b
+# dzielenie_calkowite = a // b
+# mnozenie = a * b
+# potegowanie = a ** b
+# potegowanie_2 = pow(a, b)
+# print(dodawanie, odejmowanie, dzielenie, dzielenie_calkowite, mnozenie, potegowanie, potegowanie_2)
+
+#zad3??????????
+
+# a = 6
+# b = 4
+#
+# a += b
+# a -= b
+# a *= b
+# a /= b
+# a **= b
+#
+# print(a)
+
+
+#zad4
+
+
+# x = pow(e, 10)
+# print(x)
+#
+# sinus = pow(sin(8), 2)
+# logarytm = 5 + sinus
+# y = pow(log(logarytm, e), 1/6)
+# print(y)
+#
+# z = 3.55
+# z2 = 4.80
+# ceiling = ceil(z)
+# floor = floor(z2)
+# print(ceiling, floor)
+
+
+
+
+#zad5
+
+# first_name = 'IGOR'
+# second_name = 'KAWCZYNSKI'
+# print('%s, %s' %(first_name.capitalize(), second_name.capitalize()))
+
+
+#zad6
+
+# string = 'bla bla bla lol lol lol lol lol lol ha ha ha ha ha'
+# print(string.count('lol'))
+
+
+
+#zad7
+
+# string = 'WydawnictwoNiemieckie'
+# print('druga : ', string[1], '\nostatnia : ', string[string.__len__()-1])
+
+
+#zad8
+
+# string = 'bla bla bla lol lol lol lol lol lol ha ha ha ha ha'
+# print(string.split())
+
+#zad9
+
+string = 'tutaj zmienna typu string'
+float = 5.8
+hexadecimal = 18
+print('%s, %f, %s' %(string, float, hex(hexadecimal)))
+
+
+
+
+
+#zad9
+
+
+
